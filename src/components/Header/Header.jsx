e008fdc8c2d99667c21aa4b5c68fc110870e0511
tamzid74 <tamzidal74@gmail.com>
2023-09-12 14:38:45 +0600

    react tailwind css file created and componend file also created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className="text-4xl font-bold ">Knowledge Cafe</h1>
+        </header>
+    );
+};
+
+export default Header;
