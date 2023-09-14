683daf00aa53655c3aabed3a61070737cab1d851
tamzid74 <tamzidal74@gmail.com>
2023-09-14 09:53:43 +0600

    added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-
+import img from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
         <header>
             <h1 className="text-4xl font-bold ">Knowledge Cafe</h1>
+            <img src={img} alt="" />
         </header>
     );
 };
---
+++ src/components/blogs/Blogs.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+
+const [blogs,setblogs] = useState([]);
+
+useEffect(()=>
+    
+,[])
+
+const Blogs = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+
+
+Blogs.propTypes ={
+    Blogs : PropTypes.array.isRequired
+}
+
+export default Blogs;
+
